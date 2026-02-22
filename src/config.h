edc986e4eabd08f2a5ed9dec196a3496ae61e06c
KallosP <pkallos19@gmail.com>
2026-02-22 17:19:31 -0500

    init

---
+++ src/config.h
@@ -0,0 +1,4 @@
+#pragma once // imports once, doesn't reimport
+#include <iostream> // basic input/output
+#include <glad/glad.h> // glad library, extra opengl functionality (gl additional dependancies)
+#include <GLFW/glfw3.h> //glfw3, spin up a window (cross platform windowing library) 
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include "config.h"
+
+// glfw = graphics library framework built on top of OpenGL: creates windows with OpenGL context, sets up callbacks, and handles timing/monitors for you
+
+// IMPORTANT NOTE: if a crash occurs and terminal output has red x in circle, but no errors in output, you might be looking at a seg fault
+
+int main(){
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout << "GLFW couldn't start" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "My Window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+    return 0;
+} 
